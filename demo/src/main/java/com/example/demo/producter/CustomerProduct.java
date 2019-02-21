8d910aef79eec57229b37ee85eaa4ca0fe0ea3d0
FuJianXin <jianxinfu@creditease.cn>
2019-02-21 16:49:00 +0800

    test

---
+++ demo/src/main/java/com/example/demo/producter/CustomerProduct.java
@@ -0,0 +1,13 @@
+package com.example.demo.producter;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author FuJianXin
+ */
+@Component
+public class CustomerProduct {
+
+
+
+}
---
+++ demo/src/main/java/com/example/demo/test/TestCal.java
@@ -0,0 +1,16 @@
+package com.example.demo.test;
+
+/**
+ * @author FuJianXin
+ * @create 2019/2/21
+ */
+public class TestCal {
+
+    public int calc(){
+        int a = 100;
+        int b = 200;
+        int c = 300;
+        return ( a + b ) * c;
+    }
+
+}
